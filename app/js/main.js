9e71a90b31cae1fc193a2c664addcc99affa7121
Dmitriy <dmitriy.tackle@gmail.com>
2021-08-03 00:26:27 +0300

    contacts page

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,17 @@ $(function () {
       autoplay: true,
    });
 
+
+
+   $('.contact-slider').slick({
+      slidesToShow: 10,
+      slidesToScroll: 10,
+      dots: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 4000,
+   });
+
    var mixer = mixitup('.gallery__inner', {
       load: {
          filter: '.living'
